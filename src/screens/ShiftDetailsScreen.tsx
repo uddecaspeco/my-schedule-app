a39dcb6344bbcbc43de6c956fe1d97c3f4b8646a
Emma Uddesson <emma.uddesson@caspeco.se>
2019-08-16 13:38:45 +0200

    Att shift detail screen content

--- src/screens/ShiftDetailsScreen.tsx
+++ src/screens/ShiftDetailsScreen.tsx
@@ -1,12 +1,20 @@
 import React from "react";
+import Moment from "moment";
+
+import "moment/locale/sv";
+
 import { Component } from "react";
 import { StyleSheet, View } from "react-native";
-import { Text } from "react-native-elements";
-
+import { Text, Header, Icon, Divider, Card, Button } from "react-native-elements";
 import { IWorkingShift } from "typings/workingShift";
-import { NavigationInjectedProps, NavigationScreenProp, NavigationState } from "react-navigation";
+import {
+  NavigationInjectedProps,
+  NavigationScreenProp,
+  NavigationState
+} from "react-navigation";
+import { colors, spacing, weight } from "../ui/theme";
 
-import { colors } from "../ui/theme";
+Moment.locale("sv");
 
 interface IShiftDetailsScreenState extends NavigationState {}
 interface IShiftDetailsScreenProps {
@@ -16,29 +24,98 @@ interface IShiftDetailsScreenProps {
 
 type Props = IShiftDetailsScreenProps & NavigationInjectedProps;
 
-
 export class ShiftDetailsScreen extends Component<Props> {
   constructor(props: Props) {
     super(props);
     this.state = {};
   }
 
-  render() {
+  renderShiftDetails = (shift: IWorkingShift) => {
     return (
       <View style={styles.container}>
-        <Text>
-          Passdetaljer
-        </Text>
+        <View style={styles.row}>
+          <Icon
+            type={"font-awesome"}
+            name={"map-marker"}
+            reverse
+            color={shift.station.color}
+            containerStyle={{ marginRight: spacing.tight }}
+          />
+          <Text h2>{shift.station.name}</Text>
+        </View>
+        <View style={styles.row}>
+          <Icon
+            type={"font-awesome"}
+            name={"calendar-o"}
+            reverse
+            color={shift.station.color}
+            containerStyle={{ marginRight: spacing.tight }}
+          />
+          <Text h2>{Moment(shift.date).format("dddd D MMMM")}</Text>
+        </View>
+        <View style={styles.row}>
+          <Icon
+            type={"font-awesome"}
+            name={"clock-o"}
+            reverse
+            color={shift.station.color}
+            containerStyle={{ marginRight: spacing.tight }}
+          />
+          <Text h2>{shift.time}</Text>
+        </View>
+        <View style={styles.commentWrapper}>
+          <Text h2 style={{ fontWeight: "bold" }}>
+            Passkommentar:
+          </Text>
+          <Card containerStyle={styles.card}>
+            {!shift.comment ? (
+              <Text style={{ fontStyle: "italic", color: colors.grey }}>
+                Ingen kommentar.
+              </Text>
+            ) : (
+              <Text>{shift.comment}</Text>
+            )}
+          </Card>
+        </View>
+      </View>
+    );
+  };
+
+  render() {
+    const shift = this.props.navigation.getParam("shift");
+    return (
+      <View style={styles.screen}>
+        <Header
+          backgroundColor={shift.station.color}
+          centerComponent={
+            <Text h1 style={{ color: colors.white }}>
+              Passdetaljer
+            </Text>
+          }
+        />
+        {this.renderShiftDetails(shift)}
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
     backgroundColor: colors.wildSand
+  },
+  card: {
+    shadowOpacity: 0,
+    marginVertical: spacing.base
+  },
+  container: {
+    padding: spacing.loose
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  commentWrapper: {
+    marginTop: spacing.loose
   }
 });
